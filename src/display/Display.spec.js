47ed7e970e638159656724bf797da6d26ff52d79
Sandrava Philips <lacasandra1@gmail.com>
2019-10-14 15:19:36 +0100

    installed dependencies

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+afterEach(rtl.cleanup);
+
+describe('Display Component', () => {
+    test("displays closed if the closed prop is true", () => {
+        const wrapper = rtl.render(<Display closed={true}/>);
+        expect(wrapper.queryByText(/closed/i)).toBeInTheDocument(); 
+           
+    });
+
+    test("displays open if the closed prop is false", () => {
+        const wrapper = rtl.render(<Display closed={false}/>);
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument(); 
+           
+    });
+})
