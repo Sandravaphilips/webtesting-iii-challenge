9b0464586b358480a25880dddf3d6eb30a292884
Sandrava Philips <lacasandra1@gmail.com>
2019-10-14 23:47:36 +0100

    dashboard tests got passed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
 // Test away
+afterEach(rtl.cleanup)
+
+test('shows the controls and display and defaults to unlocked and open', () => {
+    const dashboard = rtl.render(<Dashboard />);
+    
+    expect(dashboard.queryByText(/open/i)).toBeInTheDocument();
+    expect(dashboard.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(dashboard.queryByText(/close gate/i)).toBeInTheDocument();
+    expect(dashboard.queryByText(/lock gate/i)).toBeInTheDocument();
+
+})
