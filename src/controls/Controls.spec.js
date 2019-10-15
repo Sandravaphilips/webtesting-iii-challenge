8bad0eb7c3a632f5b096d6f11979fecdbc9be150
Sandrava Philips <lacasandra1@gmail.com>
2019-10-15 13:43:03 +0100

    MVP completed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,45 @@
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from './Controls';
+
 // Test away!
+afterEach(rtl.cleanup);
+
+describe('Controls Component', () => {
+    it('provide buttons to toggle the `closed` and `locked` states', () => {
+        const control = rtl.render(<Controls />);
+        expect(control.queryByText(/close gate/i)).toBeInTheDocument();
+        expect(control.queryByText(/lock gate/i)).toBeInTheDocument();
+    })
+
+    it('close button text changes to reflect the state the door will be in if clicked', () => {
+        const control = rtl.render(<Controls closed={false} locked={false}/>);
+        
+        let closeGateElement = control.queryByText(/close gate/i);
+        rtl.fireEvent.click(closeGateElement);
+        expect(control.queryByText(/lock gate/i)).toBeInTheDocument();
+        
+    })
+
+    it('lock button text changes to reflect the state the door will be in if clicked', () => {
+        const control = rtl.render(<Controls closed={true} locked={true}/>);
+        
+        let lockGateElement = control.queryByText(/unlock gate/i);
+        rtl.fireEvent.click(lockGateElement);
+        expect(control.queryByText(/lock gate/i)).toBeInTheDocument();
+        
+    })
+
+    it('the locked toggle button is disabled if the gate is open', () => {
+        const control = rtl.render(<Controls />);
+        let button = control.queryByText(/lock gate/i);
+        expect(button.disabled).toBe(true);
+    });
+
+    it('the closed toggle button is disabled if the gate is locked', () => {
+        const control = rtl.render(<Controls closed={true} locked={true}/>);
+        let button = control.queryByText(/open gate/i);
+        expect(button.disabled).toBe(true);
+    })
+})
